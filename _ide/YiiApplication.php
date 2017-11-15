bb414a117448a30eb75438a78bcf8ad80eee67de
semenov <semenov@skeeks.com>
2017-11-15 22:16:14 +0300

    code style

--- _ide/YiiApplication.php
+++ _ide/YiiApplication.php
@@ -8,14 +8,16 @@
  * @copyright 2010 SkeekS (СкикС)
  * @date 10.09.2015
  */
+
 namespace yii\web;
+
 use skeeks\cms\search\CmsSearchComponent;
 
 /**
- * @property CmsSearchComponent                    $cmsSearch
-
+ * @property CmsSearchComponent $cmsSearch
  * Class Application
  * @package yii\web
  */
 class Application
-{}
+{
+}
